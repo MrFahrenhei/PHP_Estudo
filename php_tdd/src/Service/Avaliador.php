8331393b0cf8de6834b93ee4b4fc78ddbcc1088e
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2023-05-22 20:46:08 -0300

    testando avaliação

--- php_tdd/src/Service/Avaliador.php
+++ php_tdd/src/Service/Avaliador.php
@@ -2,19 +2,50 @@
 
 namespace Alura\Leilao\Service;
 
+use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private $maiorValor;
+    private float $menorValor = INF;
+    private float $maiorValor = -INF;
+    private array $maioresLances;
+
+    public function __construct(
+    ){
+        $maiorValor = $this->maiorValor;
+        $menorValor = $this->menorValor;
+        $maioresLances = $this->maioresLances;
+    }
+
     public function avalia(Leilao $leilao): void
     {
+        foreach($leilao->getLances() as $lance){
+            if($lance->valor > $this->maiorValor){
+                $this->maiorValor = $lance->valor;
+            }
+            if($lance->valor < $this->menorValor){
+                $this->menorValor = $lance->valor;
+            }
+        }
         $lances = $leilao->getLances();
-        $ultimoLance = $lances[count($lances) - 1];
-        $this->maiorValor = $ultimoLance->valor;
+        usort($lances, function (Lance $lance1, Lance $lance2){
+            return $lance1->valor - $lance2->valor;
+        });
+        $this->maioresLances = array_slice($lances, 0, 3);
     }
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
+    }
+
+    public function getMaioresLances(): array
+    {
+        return $this->maioresLances;
     }
 }
--- php_tdd/tests/Alura/Leilao/Tests/Service/AvaliadorTest.php
+++ php_tdd/tests/Alura/Leilao/Tests/Service/AvaliadorTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class AvaliadorTest extends TestCase
 {
-    public function testEncontrarOMaiorValor()
+    public function testEncontrarOMaiorValorCrescente()
     {
         // Arrange - Given
         $leilao = new Leilao("Fiat 147 0KM");
@@ -28,4 +28,59 @@ class AvaliadorTest extends TestCase
 
         self::assertEquals(2500, $maiorValor);
     }
+    public function testEncontrarOMaiorValorDecrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao("Fiat 147 0KM");
+        $maria = new Usuario('Maria');
+        $joao = new Usuario("João");
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leiloeiro = new Avaliador();
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+        // Assert - Then
+
+        self::assertEquals(2500, $maiorValor);
+    }
+    public function testEncontrarMenorValorCrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao("Fiat 147 0KM");
+        $maria = new Usuario('Maria');
+        $joao = new Usuario("João");
+
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leiloeiro = new Avaliador();
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $menorValor = $leiloeiro->getMenorValor();
+        // Assert - Then
+
+        self::assertEquals(2000, $menorValor);
+    }
+    public function testEncontrarMenorValorDecrescente()
+    {
+        // Arrange - Given
+        $leilao = new Leilao("Fiat 147 0KM");
+        $maria = new Usuario('Maria');
+        $joao = new Usuario("João");
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leiloeiro = new Avaliador();
+        // Act - When
+        $leiloeiro->avalia($leilao);
+
+        $menorValor = $leiloeiro->getMenorValor();
+        // Assert - Then
+
+        self::assertEquals(2000, $menorValor);
+    }
+
 }
